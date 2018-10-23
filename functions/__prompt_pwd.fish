74abdf8d49531e8bb4102a94d99752ce80b55bbe
Roman Coedo <romancoedo@gmail.com>
2018-10-23 09:40:58 +0200

    Add gitlab to abbreviations

--- functions/__prompt_pwd.fish
+++ functions/__prompt_pwd.fish
@@ -1,3 +1,3 @@
 function __prompt_pwd
-  echo $PWD | sed -e "s|^$GHQ_ROOT\/github\.com|gh|" | sed -e "s|^$GHQ_ROOT\/bitbucket\.org|bb|" | sed -e "s|^$HOME|~|"
+  echo $PWD | sed -e "s|^$GHQ_ROOT\/github\.com|gh|" | sed -e "s|^$GHQ_ROOT\/gitlab\.com|gl|" | sed -e "s|^$GHQ_ROOT\/bitbucket\.org|bb|" | sed -e "s|^$HOME|~|"
 end
